696920ce2cf5ebde8264dd5c4d1cf60a7d1f5e67
Andrea Gastaldello <andreagast@msn.com>
2012-09-02 15:49:14 +0200

    add: added DecimalCellRenderer.

---
+++ add/src/main/java/it/gas/altichierock/add/DecimalCellRenderer.java
@@ -0,0 +1,26 @@
+package it.gas.altichierock.add;
+
+import java.text.DecimalFormat;
+
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class DecimalCellRenderer extends DefaultTableCellRenderer {
+	private static final long serialVersionUID = 1L;
+	private DecimalFormat format;	
+	
+	public DecimalCellRenderer() {
+		//format = new DecimalFormat("0.00");
+	}
+
+	@Override
+	protected void setValue(Object arg0) {
+		if (format == null)
+			format = new DecimalFormat("0.00");
+		if (arg0 == null)
+			setText("");
+		setText(format.format(arg0));
+		System.out.println("lol");
+	}
+
+	
+}
