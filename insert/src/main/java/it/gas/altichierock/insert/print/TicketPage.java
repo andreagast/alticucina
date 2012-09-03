6eb045e11b8bbf215e879430e7f5c11c0fdc2138
Andrea Gastaldello <andreagast@msn.com>
2012-09-04 00:18:53 +0200

    insert: correctly formatted the print page.

--- insert/src/main/java/it/gas/altichierock/insert/print/TicketPage.java
+++ insert/src/main/java/it/gas/altichierock/insert/print/TicketPage.java
@@ -12,9 +12,15 @@ import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.text.DecimalFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TicketPage implements Printable {
+	private static final int HEADER_ROWS = 3;
+	private static final int FOOTER_ROWS = 2;
 	private Ticket ticket;
 	private DecimalFormat format;
+	private Logger log = LoggerFactory.getLogger(TicketPage.class);
 
 	public TicketPage(Ticket t) {
 		this.ticket = t;
@@ -27,38 +33,58 @@ public class TicketPage implements Printable {
 		Font font = new Font("Serif", Font.PLAIN, 10);
 		FontMetrics metrics = g.getFontMetrics(font);
 		int lineHeight = metrics.getHeight();
+
 		int linesPerPage = (int) (pf.getImageableHeight() / lineHeight);
-		int lines = 2 + ticket.getContent().size();
-		int pages = lines / linesPerPage;
-		if (lines % linesPerPage != 0)
-			pages++;
-		if (pageIndex > pages - 1)
+		log.debug("linesPerPage: " + linesPerPage);
+		int totalLines = HEADER_ROWS + ticket.getContent().size() + FOOTER_ROWS;
+		int totalPages = totalLines / linesPerPage;
+		if (totalLines % linesPerPage != 0)
+			totalPages++;
+		log.debug("totalPages: " + totalPages);
+		if (pageIndex > totalPages - 1)
 			return NO_SUCH_PAGE;
+
+		// calculate lines to draw in this page
+		int thisLines = 0;
+		thisLines = totalLines - (linesPerPage * pageIndex);
+		if (thisLines > linesPerPage)
+			thisLines = linesPerPage;
 
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.translate(pf.getImageableX(), pf.getImageableY());
+		
 		int start = pageIndex * linesPerPage;
 		int y = 0;
-		for (int i = 0; i <= linesPerPage; i++) {
-			//System.out.println("Ciclo! " + pageIndex + ":" + i);
+		for (int i = 0; i <= thisLines - 1; i++) {
+			log.debug(pageIndex + ":" + i);
+			// log.debug("Ciclo! " + pageIndex + ":" + i);
 			y += lineHeight;
+			// draw header & footer
 			if (pageIndex == 0 && i == 0) {
 				g.drawString("Ticket n. " + ticket.getTicketid(), 0, y);
 			} else if (pageIndex == 0 && i == 1) {
 				g.drawString("Date: " + ticket.getCreateDate() + " - Time: "
 						+ ticket.getCreateTime(), 0, y);
+			} else if (pageIndex == 0 && i == 2) {
+				g.drawString("", 0, y);
+			} else if (pageIndex == totalPages - 1 && i == thisLines - 2) {
+				g.drawString("--------", 0, y);
+			} else if (pageIndex == totalPages - 1 && i == thisLines - 1) {
+				g.drawString("TOTAL:", 0, y);
 			} else {
+				// draw content
 				try {
-					TicketContent tc = ticket.getContent().get(start + i - 2);
+					// remember to skip header rows
+					TicketContent tc = ticket.getContent().get(
+							start + i - HEADER_ROWS);
 					String qty = tc.getQuantity() == 0 ? "" : Integer
 							.toString(tc.getQuantity());
 					String descr = tc.getDescription();
-					//System.out.println(descr);
 					String price = tc.getPrice() == 0 ? "" : format.format(tc
 							.getPrice());
 					g.drawString(qty + "\t" + descr + "\t" + price, 0, y);
 				} catch (IndexOutOfBoundsException e) {
-					g.drawString("", 0, y);
+					log.warn("This should not happen.", e);
 				}
 			}
 		}
