c0bd2f2e1595342c407fbb950d4c8fd303ed357d
Andrea Gastaldello <andreagast@msn.com>
2012-10-03 10:38:34 +0200

    Renamed project to "alticucina".

--- database/src/main/java/it/gas/altichierock/database/Constants.java
+++ database/src/main/java/it/gas/altichierock/database/Constants.java
@@ -1,6 +1,6 @@
 package it.gas.altichierock.database;
 
 public interface Constants {
-	public final static String TITLE = "AltichieRock";
+	public final static String TITLE = "AltiCucina";
 	public final static String TWITTER = "@andreagast";
 }
